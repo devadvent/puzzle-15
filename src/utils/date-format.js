7e8f69169b9d25e12eedb130dd381a79b72ca6c9
Marc Backes <hello@marc.dev>
2021-12-15 19:29:59 +0100

    initial commit 🎅

---
+++ src/utils/date-format.js
@@ -0,0 +1,7 @@
+export const countdown = (date, referenceDate = new Date()) => {
+    return { days: 0, hours: 0, minutes: 0, seconds: 0 }
+}
+
+export const formatCountdown = countdown => {
+    return ''
+}
